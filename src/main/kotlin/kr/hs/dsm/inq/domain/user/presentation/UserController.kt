96f29e122aa63ac8d0ab26d06514e8d13242fc99
woonseok105 <yws3786@gmail.com>
2023-10-24 15:58:44 +0900

    fix: 빌드 에러 재수정

--- src/main/kotlin/kr/hs/dsm/inq/domain/user/presentation/UserController.kt
+++ src/main/kotlin/kr/hs/dsm/inq/domain/user/presentation/UserController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import javax.validation.Valid
+import javax.validation.Valid as Valid
 
 @RequestMapping("/user")
 @RestController
